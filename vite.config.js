35d73edd1789cd493d48bf0e0c8f9b48c14e1b02
abdixassan <ahakiimmooliid@gmail.com>
2025-11-11 14:43:30 +0300

    animation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteSSG } from 'vite-plugin-ssg'
-
 export default defineConfig({
-  plugins: [react(), ViteSSG()]
+  plugins: [react()]
 })

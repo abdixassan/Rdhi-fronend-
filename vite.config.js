380ac8779ca56193687790ce4f49cd9328185c34
abdixassan <ahakiimmooliid@gmail.com>
2025-11-12 16:49:12 +0300

    build success - update config

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // waa in la kordhiyo limit-ka 500KB -> 2000KB
+  },
 })
